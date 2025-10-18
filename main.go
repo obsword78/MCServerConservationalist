0dcacafae5e1b49819a4bc30c5b4deea788f37e2
obsword78 <obsword78@gmail.com>
2025-10-18 23:13:35 +0800

    reuse reader and open listener permanently

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-
-	// "time"
-
 	"bufio"
 	"fmt"
 	"net"
@@ -16,38 +13,30 @@ import (
 var yamlCfg *lib.YAMLConfig
 var srvProps *lib.ServerProps
 var serverRunning = new(int32)
+var serverStarted = make(chan struct{}) // channel trigger
 
 func main() {
 	yamlCfg = lib.LoadYAMLConfig("MCServerConservationalist.yaml")
 	srvProps = lib.LoadServerProps("server.properties")
-	atomic.StoreInt32(serverRunning, 0)
 
-	for {
-		WaitForValidTrigger()
-		MonitorIdle()
-	}
-}
-
-func WaitForValidTrigger() {
-	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", srvProps.ServerPort))
-	if err != nil {
-		fmt.Println("Error creating listener:", err)
-		return
-	}
+	ln, _ := net.Listen("tcp", fmt.Sprintf(":%d", srvProps.ServerPort))
 	defer ln.Close()
 
 	for {
-		if atomic.LoadInt32(serverRunning) == 1 {
-            fmt.Println("Server start triggered → stopping listener")
-            break
-        }
+		atomic.StoreInt32(serverRunning, 0)
+		serverStarted = make(chan struct{})
 
-		conn, err := ln.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection:", err)
-			continue
-		}
-		go handleConnection(conn) // handle each client concurrently
+		fmt.Println("Waiting for valid trigger...")		
+		go func() {
+			for {
+				conn, _ := ln.Accept()
+				go handleConnection(conn)
+			}
+		}()
+
+		<-serverStarted
+		fmt.Println("Server triggered → starting idle monitor")
+		MonitorIdle()
 	}
 }
 
@@ -55,6 +44,7 @@ func MonitorIdle() {
 	idleSeconds := 0
 	for {
 		count := lib.GetPlayerCountRCON(fmt.Sprintf("localhost:%d", srvProps.RconPort), srvProps.RconPassword)
+		fmt.Println(count)
 		if count <= 0 {
 			idleSeconds++
 			if idleSeconds >= yamlCfg.IdleTimeoutSeconds {
@@ -72,6 +62,12 @@ func MonitorIdle() {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
+
+	if atomic.LoadInt32(serverRunning) == 1 {
+		fmt.Println("Server already running, rejecting login attempt")
+		return
+	}
+
 	fmt.Println("Client connected:", conn.RemoteAddr())
 
 	r := bufio.NewReader(conn)
@@ -89,8 +85,15 @@ func handleConnection(conn net.Conn) {
 	case 2: // Login
 		if atomic.LoadInt32(serverRunning) == 1 {
 			fmt.Println("Server already running, rejecting login attempt")
-		} else if err := lib.HandleLogin(conn, yamlCfg, serverRunning); err != nil {
-			fmt.Println("Status error:", err)
+		} else {
+			if err := lib.HandleLogin(r, conn, yamlCfg, serverRunning); err != nil {
+				fmt.Println("Login error:", err)
+			}
+			select {
+			case <-serverStarted:
+			default:
+				close(serverStarted)
+			}
 		}
 	default:
 		fmt.Println("Unknown next state:", hs.NextState)
